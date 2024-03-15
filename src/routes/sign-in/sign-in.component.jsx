5205d333a10fe31b8932fca027a7be17d4190d30
Sur Prakash <prakashsur85@gmail.com>
2024-03-15 08:47:17 +0530

    created sign up component, the form input component, button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,4 +1,9 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { 
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth 
+   } from '../../utils/firebase/firebase.utils'
+
+import SignupForm from '../../components/sign-up-form/sign-up-form.components';
 
 const SignIn = () => {
   const logGoogleUser = async () => {
@@ -10,6 +15,7 @@ const SignIn = () => {
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <SignupForm />
         </div>
     );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,10 +1,12 @@
 import { initializeApp } from 'firebase/app';
 import { 
-  getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider 
+  getAuth, 
+  signInWithRedirect, 
+  signInWithPopup, 
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword
   } from 'firebase/auth';
-import {
-  getFirestore, doc, getDoc, setDoc
-} from 'firebase/firestore'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyAZJt6cPpoaDzKEKTtgvVeJemcGZle1qmc",
@@ -17,18 +19,20 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleprovider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleprovider.setCustomParameters({
     prompt: "select_account"
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleprovider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleprovider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+  if(!userAuth) return; 
   const userDocRef = doc(db, 'users', userAuth.uid);
 
   const userSnapshot = await getDoc(userDocRef);
@@ -41,7 +45,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
       await setDoc(userDocRef, {
         displayName,
         email,
-        createdAt
+        createdAt,
+        ...additionalInformation
       });
     } catch (error) {
       console.log('error creating the user', error.meesage);
@@ -49,4 +54,10 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   }
 
   return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return; 
+
+  return await createUserWithEmailAndPassword(auth, email, password); // Fixed function call here (createUserWithEmailAndPassword instead of createAuthUserWithEmailAndPassword)
 };
