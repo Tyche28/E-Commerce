b06ae1d0ad691a1dbf9029cf22160376ee39d53c
Sur Prakash <prakashsur85@gmail.com>
2024-03-17 14:03:11 +0530

    created authentication page component alongside sign in form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,7 +4,8 @@ import {
   signInWithRedirect, 
   signInWithPopup, 
   GoogleAuthProvider,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
   } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
@@ -59,5 +60,12 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return; 
 
-  return await createUserWithEmailAndPassword(auth, email, password); // Fixed function call here (createUserWithEmailAndPassword instead of createAuthUserWithEmailAndPassword)
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return; 
+
+  return await signInWithEmailAndPassword(auth, email, password);
 };
